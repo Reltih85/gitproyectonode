c3077783ec9479c92562b4dc55b11f55bef5fb48
Reltih85 <reltihfybp@gmail.com>
2024-08-31 18:21:57 -0500

    Actualizado mas consultas

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const productos = [
     { id: 1, nombre: 'Producto 1', precio: 10 },
     { id: 2, nombre: 'Producto 2', precio: 20 },
-    { id: 3, nombre: 'Producto 3', precio: 300 }
-  ];
+    { id: 3, nombre: 'Producto 3', precio: 30 }
+];
 
 app.get('/productos', (req, res) => {
-res.json(productos);
+    res.json(productos);
+});
+
+app.get('/productos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const producto = productos.find(p => p.id === id);
+    if (producto) {
+        res.json(producto);
+    } else {
+        res.status(404).send('Producto no encontrado');
+    }
+});
+
+app.post('/productos', (req, res) => {
+    const nuevoProducto = req.body;
+    productos.push(nuevoProducto);
+    res.status(201).json(nuevoProducto);
+});
+
+app.put('/productos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    let producto = productos.find(p => p.id === id);
+    if (producto) {
+        Object.assign(producto, req.body);
+        res.json(producto);
+    } else {
+        res.status(404).send('Producto no encontrado');
+    }
+});
+
+ app.delete('/productos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = productos.findIndex(p => p.id === id);
+    if (index !== -1) {
+        productos.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send('Producto no encontrado');
+    }
 });
 
 const port = process.env.PORT || 3000;
