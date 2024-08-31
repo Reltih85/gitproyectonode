8f376cbb3973e6441175b65ee47492d60c04f5bc
Reltih85 <reltihfybp@gmail.com>
2024-08-31 15:02:43 -0500

    Descripción clara de la modificación realizada

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const productos = [
     { id: 1, nombre: 'Producto 1', precio: 10 },
     { id: 2, nombre: 'Producto 2', precio: 20 },
-    { id: 3, nombre: 'Producto 3', precio: 30 }
+    { id: 3, nombre: 'Producto 3', precio: 300 }
   ];
 
 app.get('/productos', (req, res) => {
